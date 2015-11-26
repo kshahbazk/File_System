3daf50bd9bfc470352de41076a75b8d6d29ee21d
benfangliu <benliuu@gmail.com>
2015-11-26 02:25:07 -0800

    added some stuff

--- src/com/cs149/IndexBlock.java
+++ src/com/cs149/IndexBlock.java
@@ -1,14 +1,18 @@
 package com.cs149;
 
+import java.util.ArrayList;
+
 public class IndexBlock {
 	private int data;
 	private IndexBlock next;
 	private int number;
+	private ArrayList<DataBlock> ptrDataBlocks = new ArrayList<>();
 
-	public IndexBlock(int data, IndexBlock next, int number)
+	public IndexBlock(int data, IndexBlock next, int number, ArrayList<DataBlock> ptrDataBlocks)
 		   {
 		      this.data = data;
 		      this.next = next;
-		      this.number =number;
+		      this.number = number;
+		      this.ptrDataBlocks = ptrDataBlocks;
 		   }
 }
--- src/com/cs149/Main.java
+++ src/com/cs149/Main.java
@@ -1,7 +1,18 @@
 package com.cs149;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
+	public ArrayList<Directory> systemDirectory = new ArrayList<Directory>();
+	
+	public LinkedList<DataBlock> unusedDataBlocks = new LinkedList<DataBlock>();
+	
+	public LinkedList<IndexBlock> unusedIndexBlocks = new LinkedList<IndexBlock>();
+	
+	
+	
     public static void main(String[] args) {
 	// write your code here
     	System.out.println("git branch test");
