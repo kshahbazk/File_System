9167bcd78676998642e711fa682c54688caac961
benfangliu <benliuu@gmail.com>
2015-11-26 02:05:09 -0800

    block

--- src/com/cs149/DataBlock.java
+++ src/com/cs149/DataBlock.java
@@ -1,5 +1,14 @@
 package com.cs149;
 
 public class DataBlock {
+	   private int data;
+	   private DataBlock next;
+	   private int number;
 
+	   public DataBlock(int data, DataBlock next, int number)
+	   {
+	      this.data = data;
+	      this.next = next;
+	      this.number =number;
+	   }
 }
--- src/com/cs149/IndexBlock.java
+++ src/com/cs149/IndexBlock.java
@@ -1,5 +1,14 @@
 package com.cs149;
 
 public class IndexBlock {
+	private int data;
+	private IndexBlock next;
+	private int number;
 
+	public IndexBlock(int data, IndexBlock next, int number)
+		   {
+		      this.data = data;
+		      this.next = next;
+		      this.number =number;
+		   }
 }
